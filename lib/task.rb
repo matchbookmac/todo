eccf302aab742605b3770785984880595d22b032
Ian MacDonald & Jesse James <student@epicodus.com>
2015-05-04 10:48:04 -0700

    create further unit spec tests and method logic for tasks

--- lib/task.rb
+++ lib/task.rb
@@ -1,8 +1,32 @@
 class Task
   attr_reader(:description)
 
-  define_method(:initialize) do |description|
-    @description = description
+  define_method(:initialize) do |attributes|
+    @description = attributes.fetch(:description)
   end
+
+  define_singleton_method(:all) do
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      tasks.push(Task.new({:description => description}))
+    end
+    tasks
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}')")
+  end
+
+  define_singleton_method(:clear) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+
+  define_method(:==) do |another_task|
+    self.description().==(another_task.description())
+  end
+
+
 
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,11 +1,42 @@
-require('rspec')
-require('task')
+require('spec_helper')
 
 describe(Task) do
   describe('#description') do
     it('returns the description of a task instance') do
-      test_task = Task.new('learn SQL')
+      test_task = Task.new({:description => 'learn SQL'})
       expect(test_task.description()).to(eq('learn SQL'))
     end
   end
+
+  describe('.all') do
+    it('return the array of all task objects') do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('save a task object to the array of task objects') do
+      test_task = Task.new({:description => 'learn SQL'})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe('.clear') do
+    it('removes all values from the array of tasks') do
+      test_task = Task.new({:description => 'learn SQL'})
+      test_task.save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it('returns true if the description of the tasks are the same') do
+      test_task_1 = Task.new({:description => 'learn SQL'})
+      test_task_2 = Task.new({:description => 'learn SQL'})
+      expect(test_task_1).to(eq(test_task_2))
+    end
+  end
+
 end
